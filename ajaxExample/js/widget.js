deebda3a05e67e91547813c41264b105df56b661
M Hughes <mhughes2011@gmail.com>
2021-03-18 19:04:15 -0700

    AJAX Example - Adding room list

--- ajaxExample/js/widget.js
+++ ajaxExample/js/widget.js
@@ -1,10 +1,10 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4) {
+  if(xhr.readyState === 4 && xhr.status === 200) {
     var employees = JSON.parse(xhr.responseText);
     var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i+=1) {
-      if(employees[i].inoffice === true) {
+    for (var i=0; i<employees.length; i += 1) {
+      if (employees[i].inoffice === true) {
         statusHTML += '<li class="in">';
       } else {
         statusHTML += '<li class="out">';
@@ -16,5 +16,28 @@ xhr.onreadystatechange = function () {
     document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
-xhr.open('GET', 'data/employees.json');
+xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+//This is the new AJAX request for the meeting room status
+
+var roomXHR = new XMLHttpRequest();
+roomXHR.onreadystatechange = function() {
+  if(roomXHR.readyState === 4 && roomXHR.status === 200) {
+    var rooms = JSON.parse(roomXHR.responseText);
+    var roomStatusHTML = '<ul class="rooms">';
+    for (let i=0; i<rooms.length; i++) {
+      if (rooms[i].available === true) {
+        roomStatusHTML += '<li class="empty">';
+      } else {
+        roomStatusHTML += '<li class="full">';
+      }
+      roomStatusHTML += rooms[i].room;
+      roomStatusHTML += '</li>';
+    }
+    roomStatusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = roomStatusHTML;
+  }
+};
+roomXHR.open('GET', '../data/rooms.json');
+roomXHR.send();
