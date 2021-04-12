a711d38096e6ae1c5925ee6dfa49405162f27851
M Hughes <mhughes2011@gmail.com>
2021-04-12 07:38:41 -0700

    changed the event handlers to a named function to implement DRY

--- jsCallbackPractice/app.js
+++ jsCallbackPractice/app.js
@@ -1,22 +1,22 @@
 const nameInput = document.getElementById('name');
 const messageTextArea = document.getElementById('message');
 
-//This event listener adds a highlight to the textfield box for nameInput
-nameInput.addEventListener('focus', event => {
+//This removed the need to rewrite the equations at every event.  You can name the function and then just use it as a variable in the event listeners.
+const focusHandler = event => {
   event.target.className = 'highlight';
-});
+};
+const blurHandler = event => {
+  event.target.className = '';
+};
+
+//This event listener adds a highlight to the textfield box for nameInput
+nameInput.addEventListener('focus', focusHandler);
 
 //This event listener removes the highlight to the textfield box for nameInput.  Blur is what happens when the cursor leaves the textfield.
-nameInput.addEventListener('blur', event => {
-  event.target.className = '';
-});
+nameInput.addEventListener('blur', blurHandler);
 
 //The same thing for the messageTextArea as above
-messageTextArea.addEventListener('focus', event => {
-  event.target.className = 'highlight';
-});
+messageTextArea.addEventListener('focus', focusHandler);
 
 //The same thing for the messageTextArea as above
-messageTextArea.addEventListener('blur', event => {
-  event.target.className = '';
-});
+messageTextArea.addEventListener('blur', blurHandler);
